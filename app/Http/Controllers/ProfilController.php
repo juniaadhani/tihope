d9a0f2719ea1bcbb49b6468620ca7e5a71c6003e
juniaadhani <junia.adhani@gmail.com>
2020-12-17 00:00:14 +0700

    profil

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -3,14 +3,21 @@
 namespace App\Http\Controllers;
 
 use App\Models\Mahasiswa;
+use http\Env\Request;
 use Illuminate\Support\Facades\DB;
 
 
 class ProfilController extends Controller
 {
-    public function index($npm_mhs)
+    public function index($npm_mhs, Request $request)
     {
+        $isLogin = $request->session()->get("isLogin");
+        $npm = $request->session()->get("npm_mhs");
+        $user = null;
+        if (!is_null($npm)) {
+            $user = DB::table("mahasiswas")->where("npm_mhs", $npm)->first();
+        }
         $mahasiswa = DB::table("mahasiswas")->where("npm_mhs", $npm_mhs)->first();
-    	return view('app/profil/profil', compact('mahasiswa'));
+    	return view('app/profil/profil', compact('mahasiswa', 'user', 'isLogin'));
     }
 }

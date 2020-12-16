1064b115c943a0c4ba72baa6ec1c79650cfb2184
juniaadhani <junia.adhani@gmail.com>
2020-12-16 20:33:15 +0700

    add login table

---
+++ database/migrations/2020_12_16_132909_create_login_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLoginTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('login', function (Blueprint $table) {
+            $table->integer('npm');
+            $table->string('password');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('login');
+    }
+}

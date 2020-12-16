635de36b984642892b5bb9ad5368e0c48a7cee05
juniaadhani <junia.adhani@gmail.com>
2020-12-16 22:23:34 +0700

    update

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -9,8 +9,23 @@ use App\Http\Controllers\Controller;
 
 class LoginController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $isLogin = $this->getCookie($request, "isLogin");
+        if ($isLogin) {
+            return view('app/home/home');
+        }
     	return view('app/login/login');
+    }
+
+    public function setCookie(Request $request, $key, $value){
+        $minutes = 360;
+        $response = new Response('Set Cookie');
+        $response->withCookie(cookie($key, $value, $minutes));
+        return $response;
+    }
+
+    public function getCookie(Request $request, $key){
+        return $request->cookie($key);
     }
 }

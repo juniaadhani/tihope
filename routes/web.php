f7b72b3a95b7645e5f1900364312bdef56540f97
juniaadhani <junia.adhani@gmail.com>
2020-12-16 22:42:09 +0700

    login test

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,4 @@ Route::get('/profil/{npm_mhs}', function ($npm_mhs) {
     return (new \App\Http\Controllers\ProfilController())->index($npm_mhs);
 });
 Route::get('/login', 'LoginController@index');
-Route::post('/login/login', 'LoginController@login')
+Route::post('/login/login', 'LoginController@login');
